5e8cec3597e97318aecfac0810d0e6bb109f2b9f
FWachter <wachterfreddy@gmail.com>
2016-06-19 16:29:02 +0200

    Adjusted form
    Added a clear form function to clear the form after submission and attempting to stop redirect on form submission

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -277,133 +277,14 @@ $("#reset").click(function() {
 /* -------------------- ----------------------- -------------------- */
 /* -------------------- Contact Form Submission -------------------- */
 /* -------------------- ----------------------- -------------------- */
-$("#contactForm").submit(function(event) {
-/* PROGRAMMER: Frederick Wachter
-   DATE CREATED: 2016-05-02
-   PURPOSE: Check for errors in the form and build the message to send using Mandrill service
+$("#formSubmit").click(function() {
+/* PROGRAMMER: Frederick Wachter - wachterfreddy@gmail.com
+   DATE CREATED: 2016-06-19
+   PURPOSE: Run function to clear form
 */
-	var errorMessage = "";
-	event.preventDefault(); // Stops URL form update
+	clearForm();
+});
 
-	if ($("#name").val() == "") {
-		errorMessage = "Please enter your name.";
-	}
-	if (!isValidEmailAddress($("#email").val())) {
-		if (errorMessage == "") {
-			errorMessage = "Please enter a valid email address.";
-		} else {
-			errorMessage = errorMessage + "<br/>Please enter a valid email address.";
-		}
-	}
-	if (!$.isNumeric($("#phone").val()) && $("#phone").val() != "") {
-		if (errorMessage == "") {
-			errorMessage = "Please enter a valid phone number.";
-		} else {
-			errorMessage = errorMessage + "<br/>Please enter a valid phone number.";
-		}
-	}
-	if ($("#contactRequest").val() == "") {
-		if (errorMessage == "") {
-			errorMessage = "Please enter a message.";
-		} else {
-			errorMessage = errorMessage + "<br/>Please enter a message.";
-		}
-	}
-
-	if (errorMessage == "") {
-		sendMail();
-		clearForm();
-		alert("Contact Request Submitted!");
-	} else {
-		$("#error").html(errorMessage);
-	}
-})
-function sendMail() {
-/* PROGRAMMER: Frederick Wachter
-   DATE CREATED: 2016-05-02
-   PURPOSE: Send mail using the Mandripp Service
-*/
-	$.ajax({
-		url: "https://mandrillapp.com/api/1.0/messages/send.json",
-		type: "POST",
-		dataType: 'json',
-		data: {
-			'key': 'CvhTgajhxOvroFDzWKu9Jw',
-			'message': {
-				'from_email': $("#email").val(),
-				'to': [{
-					'email': 'f.wachter@hotmail.com',
-					'name': $("#name").val(),
-					'type': 'to'
-				}],
-				'autotext': 'true',
-				'subject': 'DSSL Contact Request',
-				'html': generateResponse()
-			}
-		}
-	}); //.done(function(response) {
-	//	console.log(response);
-	//});
-}
-function generateResponse() {
-/* PROGRAMMER: Frederick Wachter
-   DATE CREATED: 2016-05-02
-   PURPOSE: Build message details for Mandrill service
-*/
-	var response = '';
-	response = 'Name: ' + $("#name").val() + '<br/>';
-	response = response + 'Email: ' + $("#email").val() + '<br/>';
-	if ($("#phone").val() == "") {
-		response = response + 'Phone Number: Phone number not given' + '<br/>';
-	} else {
-		response = response + 'Phone Number: ' + $("#phone").val() + '<br/>';
-	}
-	response = response + 'Date: ' + currentDate() + '<br/>';
-	response = response + 'Time: ' + currentTime() + '<br/><br/>';
-	response = response + 'Message: <br/>' + $("#contactRequest").val();
-	return response;
-}
-function currentDate() {
-/* PROGRAMMER: Frederick Wachter
-   DATE CREATED: 2016-05-02
-   PURPOSE: Get the current date
-*/
-	var currentDate = new Date;
-	var Day = currentDate.getDate();
-	if (Day < 10) {
-		Day = '0' + Day;
-	}
-	var Month = currentDate.getMonth() + 1;
-	if (Month < 10) {
-		Month = '0' + Month;
-	}
-	var Year = currentDate.getFullYear();
-	var fullDate = Month + '/' + Day + '/' + Year;
-	return fullDate;
-}
-function currentTime() {
-/* PROGRAMMER: Frederick Wachter
-   DATE CREATED: 2016-05-02
-   PURPOSE: Get the current time
-*/
-	var currentTime = new Date;
-	var Minutes = currentTime.getMinutes();
-	if (Minutes < 10) {
-		Minutes = '0' + Minutes;
-	}
-	var Hour = currentTime.getHours();
-	if (Hour > 12) {
-		Hour -= 12;
-	}
-	var Time = Hour + ':' + Minutes;
-	if (currentTime.getHours() <= 12) {
-		Time += ' AM';
-	}
-	if (currentTime.getHours() > 12) {
-		Time += ' PM';
-	}
-	return Time;
-}
 function clearForm() {
 /* PROGRAMMER: Frederick Wachter
    DATE CREATED: 2016-05-02
@@ -414,14 +295,6 @@ function clearForm() {
 	for (var i = 0; i < l; ++i) {
 		elems[i].value = "";
 	}
-
-	elems = document.getElementsByTagName("textarea");
-	var l = elems.length;
-	for (var i = 0; i < l; ++i) {
-		elems[i].value = "";
-	}
-
-	$("#error").html("");
 }
 
 
