7810a978fcd764c6c7e5cc1614c408fc17a5c4ef
FWachter <wachterfreddy@gmail.com>
2016-09-22 21:38:21 -0400

    Updated lab manager, contact us email, added analytics code

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -284,7 +284,7 @@ $("#contactUs").submit(function(e) {
 */
 	e.preventDefault();
 	$.ajax({
-	    url: "https://formspree.io/f.wachter@hotmail.com", 
+	    url: "https://formspree.io/wachterfreddy@gmail.com", 
 	    method: "POST",
 	    data: {name: $("#userName").val(), _replyto: $("#userEmail").val()},
 	    dataType: "json"
@@ -306,5 +306,16 @@ function clearForm() {
 		elems[i].value = "";
 	}
 }
+
+/* -------------------- ---------------- -------------------- */
+/* -------------------- Google Analytics -------------------- */
+/* -------------------- ---------------- -------------------- */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-66023611-1', 'auto');
+ga('send', 'pageview');
 
 
